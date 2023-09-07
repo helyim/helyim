44fbf64a102f8c82c41062b47f79e08c1b019fac
iamazy <iamazy.me@outlook.com>
2023-09-07 23:34:54 +0800

    feat: add favicon handler

--- helyim/src/images/mod.rs
+++ helyim/src/images/mod.rs
@@ -1,37 +1,27 @@
-use std::{cell::OnceCell, path::Path};
+use std::cell::OnceCell;
 
 use bytes::Bytes;
-use tracing::error;
 
 use crate::errors::Result;
 
 pub struct Favicon {
-    path: &'static str,
     cell: OnceCell<Bytes>,
 }
 
 unsafe impl Sync for Favicon {}
 
 impl Favicon {
-    pub const fn new(path: &'static str) -> Self {
+    pub const fn new() -> Self {
         Self {
-            path,
             cell: OnceCell::new(),
         }
     }
     pub fn bytes(&self) -> Result<&[u8]> {
-        let buf = self.cell.get_or_init(|| {
-            let path = Path::new("src/images").join(self.path);
-            match std::fs::read(&path) {
-                Ok(image) => Bytes::from(image),
-                Err(err) => {
-                    error!("loading favicon.ico failed. {err}");
-                    Bytes::new()
-                }
-            }
-        });
+        let buf = self
+            .cell
+            .get_or_init(|| Bytes::from_static(include_bytes!("favicon/favicon.ico")));
         Ok(buf)
     }
 }
 
-pub static FAVICON_ICO: Favicon = Favicon::new("favicon/favicon.ico");
+pub static FAVICON_ICO: Favicon = Favicon::new();
