de1fb97e1d084bc19f120440814bfe94a1d477cb
iamazy <iamazy.me@outlook.com>
2023-11-12 00:20:53 +0800

    chore: clean code

--- helyim/src/topology/mod.rs
+++ helyim/src/topology/mod.rs
@@ -1,14 +1,10 @@
 pub mod collection;
 
 mod data_center;
-pub use data_center::DataCenter;
-
 mod data_node;
 pub use data_node::DataNodeRef;
 
 mod rack;
-pub use rack::Rack;
-
 #[allow(clippy::module_inception)]
 mod topology;
 pub use topology::{topology_loop, topology_vacuum_loop, Topology, TopologyEventTx};
