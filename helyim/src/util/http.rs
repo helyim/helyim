8add513178ca692a33697495a6aee2481552445b
iamazy <iamazy.me@outlook.com>
2023-11-25 22:04:29 +0800

    feat(http): support `Query`  in `FormOrJson`

--- helyim/src/util/http.rs
+++ helyim/src/util/http.rs
@@ -2,7 +2,7 @@ use std::time::Duration;
 
 use axum::{
     body::HttpBody,
-    extract::FromRequest,
+    extract::{FromRequest, Query},
     http::{header::CONTENT_TYPE, StatusCode},
     response::IntoResponse,
     BoxError, Form, Json, RequestExt,
@@ -10,6 +10,7 @@ use axum::{
 use bytes::Bytes;
 use hyper::{body::to_bytes, header::CONTENT_LENGTH, Body, Client, Method, Request};
 use serde::de::DeserializeOwned;
+use tracing::error;
 use url::Url;
 
 use crate::errors::Result;
@@ -70,28 +71,51 @@ where
         req: Request<B>,
         _state: &S,
     ) -> std::result::Result<Self, Self::Rejection> {
-        let content_type = req
-            .headers()
-            .get(CONTENT_TYPE)
-            .and_then(|value| value.to_str().ok())
-            .ok_or_else(|| StatusCode::BAD_REQUEST.into_response())?;
+        match req.method() {
+            &Method::GET | &Method::HEAD => {
+                let Query(payload) = req
+                    .extract::<Query<T>, _>()
+                    .await
+                    .map_err(|err| err.into_response())?;
 
-        if content_type.starts_with("application/json") {
-            let Json(payload) = req
-                .extract::<Json<T>, _>()
-                .await
-                .map_err(|err| err.into_response())?;
+                return Ok(Self(payload));
+            }
+            &Method::POST => {
+                let content_type = req
+                    .headers()
+                    .get(CONTENT_TYPE)
+                    .and_then(|value| value.to_str().ok())
+                    .ok_or_else(|| StatusCode::BAD_REQUEST.into_response())?;
 
-            Ok(Self(payload))
-        } else if content_type.starts_with("application/x-www-form-urlencoded") {
-            let Form(payload) = req
-                .extract::<Form<T>, _>()
-                .await
-                .map_err(|err| err.into_response())?;
+                if content_type.starts_with("application/json") {
+                    let Json(payload) = req
+                        .extract::<Json<T>, _>()
+                        .await
+                        .map_err(|err| err.into_response())?;
 
-            Ok(Self(payload))
-        } else {
-            Err(StatusCode::BAD_REQUEST.into_response())
+                    return Ok(Self(payload));
+                } else if content_type.starts_with("application/x-www-form-urlencoded") {
+                    let Form(payload) = req
+                        .extract::<Form<T>, _>()
+                        .await
+                        .map_err(|err| err.into_response())?;
+
+                    return Ok(Self(payload));
+                } else {
+                    error!(
+                        "invalid http request: url: {}, method: {}, content type: {content_type}",
+                        req.uri(),
+                        req.method()
+                    );
+                }
+            }
+            _ => error!(
+                "invalid http request: url: {}, method: {}",
+                req.uri(),
+                req.method()
+            ),
         }
+
+        Err(StatusCode::BAD_REQUEST.into_response())
     }
 }
