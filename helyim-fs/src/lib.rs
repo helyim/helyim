db228cfc05ff8ae7973066bb0170e8a9f70c942a
iamazy <iamazy.me@outlook.com>
2024-01-02 23:54:32 +0800

    Feat(iouring): support async file operations

---
+++ helyim-fs/src/lib.rs
@@ -0,0 +1,259 @@
+#[cfg(not(all(target_os = "linux", feature = "iouring")))]
+use std::os::unix::fs::FileExt;
+
+#[cfg(all(target_os = "linux", feature = "iouring"))]
+use tokio_uring::buf::IoBuf;
+
+pub struct File {
+    #[cfg(not(all(target_os = "linux", feature = "iouring")))]
+    inner: std::fs::File,
+
+    #[cfg(all(target_os = "linux", feature = "iouring"))]
+    inner: tokio_uring::fs::File,
+}
+
+impl File {
+    pub fn new(file: std::fs::File) -> Self {
+        cfg_if::cfg_if! {
+            if #[cfg(all(target_os = "linux", feature = "iouring"))] {
+                Self {
+                    inner: tokio_uring::fs::File::from_std(file)
+                }
+            } else {
+                Self {
+                    inner: file
+                }
+            }
+        }
+    }
+
+    #[allow(unused_mut)]
+    pub async fn read_at(&self, mut buf: Vec<u8>, offset: u64) -> BufResult<usize, Vec<u8>> {
+        cfg_if::cfg_if! {
+            if #[cfg(all(target_os = "linux", feature = "iouring"))] {
+                self.inner.read_at(buf, offset).await
+            } else {
+                let read_at = self.inner.read_at(&mut buf, offset);
+                (read_at, buf)
+            }
+        }
+    }
+
+    /// Read the exact number of bytes required to fill `buf` at the specified
+    /// offset from the file.
+    ///
+    /// This function reads as many as bytes as necessary to completely fill the
+    /// specified buffer `buf`.
+    ///
+    /// # Return
+    ///
+    /// The method returns the operation result and the same buffer value passed
+    /// as an argument.
+    ///
+    /// If the method returns [`Ok(())`], then the read was successful.
+    ///
+    /// # Errors
+    ///
+    /// If this function encounters an error of the kind
+    /// [`ErrorKind::Interrupted`] then the error is ignored and the
+    /// operation will continue.
+    ///
+    /// If this function encounters an "end of file" before completely filling
+    /// the buffer, it returns an error of the kind
+    /// [`ErrorKind::UnexpectedEof`]. The buffer is returned on error.
+    ///
+    /// If this function encounters any form of I/O or other error, an error
+    /// variant will be returned. The buffer is returned on error.
+    ///
+    /// refer to https://github.com/bytedance/monoio/blob/4c3fed563600b4b9de8f9526f1ddd897a797e31f/monoio/src/fs/file.rs#L194-L274
+    pub async fn read_exact_at(&self, mut buf: Vec<u8>, offset: u64) -> BufResult<(), Vec<u8>> {
+        cfg_if::cfg_if! {
+            if #[cfg(all(target_os = "linux", feature = "iouring"))] {
+                let len = buf.bytes_total();
+                let mut read = 0;
+                while read < len {
+                    let slice = buf.slice(read..len);
+                    let (res, slice) = self.inner.read_at(slice, offset + read as u64).await;
+                    buf = slice.into_inner();
+                    match res {
+                        Ok(0) => {
+                            return (
+                                Err(std::io::Error::new(
+                                    std::io::ErrorKind::UnexpectedEof,
+                                    "failed to fill whole buffer",
+                                )),
+                                buf,
+                            )
+                        }
+                        Ok(n) => {
+                            read += n;
+                        }
+                        Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => {}
+                        Err(e) => return (Err(e), buf),
+                    };
+                }
+                (Ok(()), buf)
+            } else {
+                let read_exact_at = self.inner.read_exact_at(&mut buf, offset);
+                (read_exact_at, buf)
+            }
+        }
+    }
+
+    pub async fn write_at(&self, buf: Vec<u8>, offset: u64) -> BufResult<usize, Vec<u8>> {
+        cfg_if::cfg_if! {
+            if #[cfg(all(target_os = "linux", feature = "iouring"))] {
+                self.inner.write_at(buf, offset).await
+            } else {
+                let write_at = self.inner.write_at(&buf, offset);
+                (write_at, buf)
+            }
+        }
+    }
+
+    /// Attempts to write an entire buffer into this file at the specified
+    /// offset.
+    ///
+    /// This method will continuously call [`write_at`] until there is no more
+    /// data to be written or an error of non-[`ErrorKind::Interrupted`]
+    /// kind is returned. This method will not return until the entire
+    /// buffer has been successfully written or such an error occurs.
+    ///
+    /// If the buffer contains no data, this will never call [`write_at`].
+    ///
+    /// # Return
+    ///
+    /// The method returns the operation result and the same buffer value passed
+    /// in as an argument.
+    ///
+    /// # Errors
+    ///
+    /// This function will return the first error of
+    /// non-[`ErrorKind::Interrupted`] kind that [`write_at`] returns.
+    ///
+    /// refer to https://github.com/bytedance/monoio/blob/4c3fed563600b4b9de8f9526f1ddd897a797e31f/monoio/src/fs/file.rs#L326-L393
+    #[allow(unused_mut)]
+    pub async fn write_all_at(&self, mut buf: Vec<u8>, offset: u64) -> BufResult<(), Vec<u8>> {
+        cfg_if::cfg_if! {
+            if #[cfg(all(target_os = "linux", feature = "iouring"))] {
+                let len = buf.bytes_init();
+                let mut written = 0;
+                while written < len {
+                    let slice = buf.slice(written..len);
+                    let (res, slice) = self.inner.write_at(slice, offset + written as u64).await;
+                    buf = slice.into_inner();
+                    match res {
+                        Ok(0) => {
+                            return (
+                                Err(std::io::Error::new(
+                                    std::io::ErrorKind::WriteZero,
+                                    "failed to write whole buffer",
+                                )),
+                                buf,
+                            )
+                        }
+                        Ok(n) => written += n,
+                        Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => {}
+                        Err(e) => return (Err(e), buf),
+                    };
+                }
+
+                (Ok(()), buf)
+            } else {
+                let write_all_at = self.inner.write_all_at(&buf, offset);
+                (write_all_at, buf)
+            }
+        }
+    }
+
+    pub async fn close(self) -> std::io::Result<()> {
+        cfg_if::cfg_if! {
+            if #[cfg(all(target_os = "linux", feature = "iouring"))] {
+                self.inner.close().await
+            } else {
+                Ok(())
+            }
+        }
+    }
+}
+
+pub type BufResult<T, B> = (std::io::Result<T>, B);
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+    use tempfile::tempfile;
+    use crate::File;
+
+    #[tokio::test]
+    #[cfg(not(all(target_os = "linux", feature = "iouring")))]
+    pub async fn test_read_exact_at() {
+        let mut std_file = tempfile().unwrap();
+        std_file.write_all(b"hello worldhello world").unwrap();
+
+        let file = File::new(std_file);
+        let buf = vec![0; 11];
+        let (_, buf) = file.read_exact_at(buf, 0).await;
+
+        assert_eq!(&buf[..], b"hello world");
+        file.close().await.unwrap();
+    }
+
+    #[test]
+    #[cfg(all(target_os = "linux", feature = "iouring"))]
+    pub fn test_read_exact_at_uring() {
+        let mut std_file = tempfile().unwrap();
+        std_file.write_all(b"hello worldhello world").unwrap();
+
+        let file = File::new(std_file);
+
+        tokio_uring::start(async {
+            let buf = vec![0; 11];
+            let (_, buf) = file.read_exact_at(buf, 0).await;
+
+            assert_eq!(&buf[..], b"hello world");
+            file.close().await.unwrap();
+        });
+    }
+
+    #[tokio::test]
+    #[cfg(not(all(target_os = "linux", feature = "iouring")))]
+    pub async fn test_write_exact_at() {
+        let mut std_file = tempfile().unwrap();
+        std_file.write_all(b"hello worldhello world").unwrap();
+
+        let file = File::new(std_file);
+
+        let buf = b"hello world".to_vec();
+        let (_, buf) = file.write_all_at(buf, 0).await;
+        assert_eq!(&buf[..], b"hello world");
+
+        let read_buf = vec![0; 11];
+        let (ret, read_buf) = file.read_exact_at(read_buf, 0).await;
+
+        assert!(ret.is_ok());
+        assert_eq!(&read_buf[..], b"hello world");
+        file.close().await.unwrap();
+    }
+
+    #[test]
+    #[cfg(all(target_os = "linux", feature = "iouring"))]
+    pub fn test_write_exact_at_uring() {
+        let std_file = tempfile().unwrap();
+        let file = File::new(std_file);
+
+        tokio_uring::start(async {
+            let buf = b"hello world".to_vec();
+            let (_, buf) = file.write_all_at(buf, 0).await;
+            assert_eq!(&buf[..], b"hello world");
+
+            let read_buf = vec![0; 11];
+            let (ret, read_buf) = file.read_exact_at(read_buf, 0).await;
+
+            assert!(ret.is_ok());
+            assert_eq!(&read_buf[..], b"hello world");
+
+            file.close().await.unwrap();
+        });
+    }
+}
