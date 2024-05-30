91de9fb57b99e1ca37d03afb864c4ade11a7fbeb
iamazy <iamazy.me@outlook.com>
2024-05-30 14:24:18 +0800

    Chore: make clippy happy

--- helyim/src/topology/volume_grow.rs
+++ helyim/src/topology/volume_grow.rs
@@ -29,9 +29,9 @@ impl VolumeGrowth {
     }
 
     /// 1. find the main data node
-    ///     1.1 collect all data nodes that have 1 slots
-    ///     2.2 collect all racks that have rp.SameRackCount+1
-    ///     2.2 collect all data centers that have DiffRackCount+rp.SameRackCount+1
+    ///  1.1 collect all data nodes that have 1 slots
+    ///  2.2 collect all racks that have rp.SameRackCount+1
+    ///  2.2 collect all data centers that have DiffRackCount+rp.SameRackCount+1
     /// 2. find rest data nodes
     async fn find_empty_slots(
         &self,
